fdecd33a306337fa538db157b087bf5e194673ec
root <wairegiwhitney11@gmail.com>
2024-01-27 07:39:25 -0800

    trial

--- 0_printf.c
+++ 0_printf.c
@@ -1,50 +1,66 @@
+
 #include "main.h"
 
-int _printf(const char *format, ...){
-    int char_count=0;
-    va_list my_args;
+/**
+ * _printf - Custom implementation of printf function
+ * @format: The format string
+ * ...: Variable arguments
+ *
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    int chars_count = 0;
+    va_list my_args_list;
 
-    if (format == NULL){
+    if (format == NULL)
+        return -1;
 
-        return (-1);
-    }
-    va_start(my_args, format);
+    va_start(my_args_list, format);
 
-    while(*format)
+    while (*format)
     {
-        if (*format!='%'){
+        if (*format != '%')
+        {
             write(1, format, 1);
-            char_count++;
+            chars_count++;
         }
         else
         {
             format++;
-            if (*format=='\0')
-                break;
-            if(*format == '%')
-            {
-              write(1, format,1);
-              char_count++;
-            }
-            else if(*format=='c')
-            {
-                char c = va_arg(my_args, int);
-                write(1, &c , 1);
-                char_count++;
-            }
-            else if(*format == 's'){
-                char *str = va_arg(my_args, char*);
-                int str_len=0;
 
-                while(str[str_len] != '\0')
+            if (*format == '\0')
+                break;
+
+            if (*format == 'c')
+            {
+                char c = va_arg(my_args_list, int);
+                write(1, &c , 1);
+                chars_count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(my_args_list, char *);
+                int str_len = 0;
+
+                while (str[str_len] != '\0')
+                {
                     str_len++;
+                }
 
                 write(1,str,str_len);
-                char_count+=str_len;
+                chars_count += str_len;
+            }
+            else if (*format == '%')
+            {
+                 write(1, format,1);
+                chars_count++;
             }
         }
+
         format++;
     }
-    va_end(my_args);
-    return char_count;
+
+    va_end(my_args_list);
+    return chars_count;
 }
