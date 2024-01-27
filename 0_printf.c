999d3827d985d621bc3030f385ad416762c86b62
Calvince Ndemo <calyndemo16@gmail.com>
2024-01-27 23:29:45 +0300

    just made it bettty compliant

--- 0_printf.c
+++ 0_printf.c
@@ -10,57 +10,58 @@
  */
 int _printf(const char *format, ...)
 {
-    int chars_count = 0;
-    va_list my_args_list;
+	int chars_count = 0;
+	va_list my_args_list;
 
-    if (format == NULL)
-        return -1;
+	if (format == NULL)
+		return (-1);
 
-    va_start(my_args_list, format);
+	va_start(my_args_list, format);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            chars_count++;
-        }
-        else
-        {
-            format++;
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			chars_count++;
+		}
+		else
+		{
+			format++;
 
-            if (*format == '\0')
-                break;
+			if (*format == '\0')
+				break;
 
-            if (*format == 'c')
-            {
-                char c = va_arg(my_args_list, int);
-                write(1, &c , 1);
-                chars_count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(my_args_list, char *);
-                int str_len = 0;
+			if (*format == 'c')
+			{
+				char c = va_arg(my_args_list, int);
 
-                while (str[str_len] != '\0')
-                {
-                    str_len++;
-                }
+				write(1, &c, 1);
+				chars_count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(my_args_list, char *);
+				int str_len = 0;
 
-                write(1,str,str_len);
-                chars_count += str_len;
-            }
-            else if (*format == '%')
-            {
-                 write(1, format,1);
-                chars_count++;
-            }
-        }
+				while (str[str_len] != '\0')
+				{
+					str_len++;
+				}
 
-        format++;
-    }
+				write(1, str, str_len);
+				chars_count += str_len;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				chars_count++;
+			}
+		}
 
-    va_end(my_args_list);
-    return chars_count;
+		format++;
+	}
+
+	va_end(my_args_list);
+	return (chars_count);
 }
