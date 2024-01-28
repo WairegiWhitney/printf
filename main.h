46885fc70a51b0a8565421c09b4943e3ef9870a9
Wairegi Whitney <137730000+WairegiWhitney@users.noreply.github.com>
2024-01-28 23:16:19 +0300

    Update main.h

--- main.h
+++ main.h
@@ -6,7 +6,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define BUFF_SIZE 1024
+#define BUFF_SIZE (1024)
+
+int _strlen(char *s);
+void stringfromint(int no, char *str);
+void dec2binstring(unsigned int no, int *chars_count);
+void rev_string(char *s, int b);
+void print_unsigned_int(int no, int *chars_count);
+void dec2octalstring(unsigned int no, int *chars_count);
+void dec2hexstring(unsigned int no, int *chars_count);
+void dec2HEXstring(unsigned int no, int *chars_count);
 
 int _printf(const char *format, ...);
 
